50a87ae90882a9507fba27991931bc70b10f747d
naleli <naleli415@gmail.com>
2025-10-13 18:36:06 -0400

    added new components Searchbar, Medialayout

--- ReviewBoardUI/src/app/app.ts
+++ ReviewBoardUI/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Searchbar } from '../searchbar/searchbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Searchbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
