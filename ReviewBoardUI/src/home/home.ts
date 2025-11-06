20c2103fa220dcef67e496259157c6749fb37ecc
naleli <naleli415@gmail.com>
2025-11-05 23:49:39 -0500

    fixed image not showing up: formatting strings with ${} needs BACKTICKS, not single or double quotes.

--- ReviewBoardUI/src/home/home.ts
+++ ReviewBoardUI/src/home/home.ts
@@ -10,12 +10,12 @@ import { MediaLayoutImage } from '../media-layout';
   styleUrl: './home.css'
 })
 export class Home {
-  readonly imageUrl = "/img";
+  readonly imageUrl = 'img';
 
   //this is called a property. you can also make this an array.
   mediaLayout: MediaLayoutImage = {
     id: 123,
-    photo: '${this.baseUrl}/photographer-at-sunset-silhouette.jpg',
+    photo: `${this.imageUrl}/photographer-at-sunset-silhouette.jpg`,
     date: '10/14/2025'
   };
 
