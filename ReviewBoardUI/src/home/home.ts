d2fc8899c56235c6ddd3b1163a0425d447363c86
naleli <naleli415@gmail.com>
2026-01-02 14:08:26 -0500

    added comments to explain functionalities around the code

--- ReviewBoardUI/src/home/home.ts
+++ ReviewBoardUI/src/home/home.ts
@@ -13,21 +13,32 @@ import { Mediaservice } from '../app/mediaservice';
 export class Home {
   readonly imageUrl = 'img';
 
+  //the base array that contains all images loaded
   mediaArr: IMediaLayout[] = [];
+  //the array that the user will see, whether they filtered on nothing or some ID
   filteredMediaArr: IMediaLayout[] = [];
+  //the service that we read our images from
   mediaService: Mediaservice = inject(Mediaservice);
 
+  //expects an ID of type string. this is called when we use the search bar
   filterResults(idNumber: string){
+    //if nothing is typed into search bar, reset filtered images to show entire array
     if(!idNumber){
       this.filteredMediaArr = this.mediaArr;
       return;
     }
 
+    //otherwise, show only the image you searched for
     this.filteredMediaArr = this.mediaArr.filter((mediaUnit) => mediaUnit.id === parseInt(idNumber));
   }
+
+  //this is where we initially set up the data/class members
   constructor() {
 
+    //populate mediaArr with data from MediaService class
     this.mediaArr = this.mediaService.getAllMedia();
+
+    //set the filtered array to that same mediaArr
     this.filteredMediaArr = this.mediaArr;
   }
 }
